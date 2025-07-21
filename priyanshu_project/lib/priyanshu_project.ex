f0c92b54ef0d4f91d094830891c117e7ecce31f0
Priyanshu singh <111607560+priyanshu6238@users.noreply.github.com>
2025-07-21 23:19:02 +0530

    Add files via upload
    Added the initial file which is created when we run mix new project.

---
+++ priyanshu_project/lib/priyanshu_project.ex
@@ -0,0 +1,18 @@
+defmodule PriyanshuProject do
+  @moduledoc """
+  Documentation for `PriyanshuProject`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PriyanshuProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ priyanshu_project/test/priyanshu_project_test.exs
@@ -0,0 +1,8 @@
+defmodule PriyanshuProjectTest do
+  use ExUnit.Case
+  doctest PriyanshuProject
+
+  test "greets the world" do
+    assert PriyanshuProject.hello() == :world
+  end
+end
