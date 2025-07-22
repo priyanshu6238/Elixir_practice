77f798f5c48ac57416b07663ba51eaa4347f646e
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-07-22 13:48:58 +0530

    Solved Question7

---
+++ priyanshu_project/test/Question7_test.exs
@@ -0,0 +1,16 @@
+defmodule Question7_test do
+  use ExUnit.Case
+  test "Question_7 functions" do
+    assert LanguageList.new()== []
+    assert LanguageList.add([],"Elixir") == ["Elixir"]
+    assert LanguageList.add(["Elixir"],"Java") == ["Java", "Elixir"]
+    assert LanguageList.remove(["Elixir", "Java"]) == ["Java"]
+    assert LanguageList.first(["Elixir", "Java"]) == "Elixir"
+    assert LanguageList.count([]) == 0
+    assert LanguageList.count(["Elixir", "Java"]) == 2
+    assert LanguageList.functional_list?(["Elixir", "Java"]) == true
+    assert LanguageList.functional_list?(["Java", "Python"]) == false
+    assert LanguageList.functional_list?([]) == false
+
+  end
+end
