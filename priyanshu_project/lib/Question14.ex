59e9f3bf2f7ff431e09816af3fc0cfc1554fb867
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-07-23 12:27:09 +0530

    Solved Question14

---
+++ priyanshu_project/lib/Question14.ex
@@ -0,0 +1,31 @@
+defmodule RPG.CharacterSheet do
+    def welcome() do
+      IO.puts("Welcome! Let's fill out your character sheet together.")
+    end
+  
+    def ask_name() do
+      IO.puts("What is your character's name?")
+      IO.gets("") |> String.trim()
+    end
+  
+    def ask_class() do
+      
+      IO.gets("What is your character's class?\n")|> String.trim()
+    end
+  
+    def ask_level() do
+      IO.gets("What is your character's level?\n")|>String.trim()|> String.to_integer()
+    end
+  
+    def run() do
+     IO.puts("Welcome! Let's fill out your character sheet together.")
+     name1=ask_name()
+     class1=ask_class()
+     level1=ask_level()
+     map=%{class: class1,level: level1, name: name1 }
+     IO.inspect(map,label: "Your character")
+     map
+         
+    end
+  end
+  
---
+++ priyanshu_project/test/Question14_test.exs
@@ -0,0 +1,10 @@
+defmodule Question14_test do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "Question14_test functions" do
+    io = capture_io(fn -> assert RPG.CharacterSheet.welcome() == :ok end)
+    assert io =="Welcome! Let's fill out your character sheet together.\n"
+    
+  end
+end
