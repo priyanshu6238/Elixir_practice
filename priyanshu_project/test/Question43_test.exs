60a9d9987960fbef29a3a4a3f4cafebc00e36552
priyanshu6238 <priyanshusingh0520@gmail.com>
2025-08-01 19:23:12 +0530

    Added test case for Question43

--- priyanshu_project/test/Question43_test.exs
+++ priyanshu_project/test/Question43_test.exs
@@ -2,7 +2,6 @@ defmodule Question43_test do
   use ExUnit.Case
 
   test "Question43_test" do
-    # Create a linked list: 1 -> 2 -> 3 -> 3 -> 4
     head = %ListNode{
       val: 1,
       next: %ListNode{
@@ -11,7 +10,6 @@ defmodule Question43_test do
       }
     }
 
-    # Expected output after removing duplicates: 1 -> 2 -> 3 -> 4
     expected = %ListNode{
       val: 2,
       next: %ListNode{val: 1, next: %ListNode{val: 4, next: %ListNode{val: 3, next: nil}}}
